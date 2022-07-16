49e7c6c72287cc0a17f69be06317ed56733bb0af
Yasuaki Uechi <y@uechi.io>
2022-07-16 17:35:59 +0900

    fix: list all papers when opening extension

--- extensions/zotero/src/commandSearchZotero.tsx
+++ extensions/zotero/src/commandSearchZotero.tsx
@@ -1,21 +1,23 @@
-import { searchResources } from "./common/zoteroApi";
+import { useEffect } from "react";
 import { useStore } from "./common/store";
 import { View } from "./common/View";
+import { searchResources } from "./common/zoteroApi";
 
 export default function MyView() {
-  const store = useStore(["results"], (_, q) => searchResources(q as string));
+  const store = useStore(["results"], (_, q) => searchResources(q as string), true);
   const sectionNames = ["Search Results"];
+
+  useEffect(() => {
+    store.runQuery("");
+  }, []);
+
   return (
     <View
       sectionNames={sectionNames}
       queryResults={store.queryResults}
       isLoading={store.queryIsLoading}
       onSearchTextChange={(text) => {
-        if (text) {
-          store.runQuery(text);
-        } else {
-          store.clearResults();
-        }
+        store.runQuery(text);
       }}
       throttle
     />
